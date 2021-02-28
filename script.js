f837930959fd9ee23c5dfb3d1102ac0023c45915
Madga Trzcinska <magdatrzcinska23@gmail.com>
2021-02-28 18:14:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const btnAdd = document.querySelector("button");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCIthAq7OP5fwoAZbgam594PB1KMnRGXjo",
+    authDomain: "student-85a0e.firebaseapp.com",
+    projectId: "student-85a0e",
+    storageBucket: "student-85a0e.appspot.com",
+    messagingSenderId: "232860863602",
+    appId: "1:232860863602:web:9e521ec9a2e3bc896146eb"
+};
+
+const table = document.querySelector('tbody');
+const tr = document.createElement("tr");
+
+firebase.initializeApp(firebaseConfig);
+let database = firebase.database();
+let ref = database.ref("student");
+let index = 0;
+
+btnAdd.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if(checkInputs()) {
+        addingToBase();
+    }
+
+});
+
+(function () {
+    ref.once("value", (snapshot) => {
+        let delay = 0;
+
+        snapshot.forEach((childSnapshot) => {
+            let data = childSnapshot.val();
+            const tr = document.createElement("tr");
+            let key = Object.keys(snapshot.val());
+            let id = parseInt(key[index])+1;
+
+            tr.innerHTML = `<th scope="row">${id}</th>
+    <td style="animation-delay:${delay}s">${data.name}</td>
+    <td style="animation-delay:${delay}s">${data.surname}</td>
+    <td style="animation-delay:${delay}s">${data.email}</td>`;
+            table.appendChild(tr);
+
+            delay += 0.1;
+            index++;
+        });
+    });
+})();
+
+function addingToBase() {
+    firebase.database().ref('student/' + index).set({
+        name: name.value,
+        surname: surname.value,
+        email: email.value
+    });
+
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `<th scope="row">${index}</th>
+        <td>${name.value}</td>
+        <td>${surname.value}</td>
+        <td>${email.value}</td>`;
+
+    table.appendChild(tr);
+    index++;
+}
---
+++ validationForm.js
@@ -0,0 +1,63 @@
+const name = document.getElementById('name');
+const surname = document.getElementById('surname');
+const email = document.getElementById('email');
+const formControl = document.querySelectorAll('.form-control');
+const small = document.querySelectorAll('small');
+
+
+function checkInputs() {
+    const usernameValue = name.value;
+    const surnameValue = surname.value;
+    const emailValue = email.value;
+    removeClasses();
+    if (usernameValue === '') {
+        errorMessage(0, "Enter a name.");
+    } else if (withoutNumbersAndSpace(usernameValue)) {
+        errorMessage(0, "Name contains a forbidden character.");
+    } else {
+        formControl[0].classList.add('success');
+    }
+
+    if (surnameValue === '') {
+        errorMessage(1, "Enter a surname.");
+    } else if (withoutNumbersAndSpace(surnameValue)) {
+        errorMessage(1, "Name contains a forbidden character.");
+    } else {
+        formControl[1].classList.add('success');
+    }
+
+
+    if (emailValue === '') {
+        errorMessage(2, "Enter a email.");
+    } else if (withoutSpace(emailValue)) {
+        errorMessage(2, "Name contains a forbidden character.");
+    } else {
+        formControl[2].classList.add('success');
+    }
+
+    if(formControl[0].classList.contains('success') && formControl[1].classList.contains('success') && formControl[2].classList.contains('success')) {
+        return true;
+    }
+}
+
+function errorMessage(id, message) {
+    formControl[id].classList.add('error');
+    small[id].innerText = message;
+}
+
+function removeClasses() {
+    formControl.forEach((e) => {
+        e.classList.remove('error');
+        e.classList.remove('success');
+    });
+}
+
+function withoutNumbersAndSpace(name) {
+    return /\s|\d/.test(name);
+}
+
+function withoutSpace(email) {
+    return /\s/.test(email);
+}
+
+
